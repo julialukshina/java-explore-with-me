f4e777c62413698c4fbd3133ba63a4362eda8f13
julialukshina <ualukshina@yandex.ru>
2022-10-04 15:57:17 +0300

    пробный 2

--- service/src/test/java/ru/yandex/practicum/service/ServiceApplicationTests.java
+++ service/src/test/java/ru/yandex/practicum/service/ServiceApplicationTests.java
@@ -2,8 +2,10 @@ package ru.yandex.practicum.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ServiceApplicationTests {
 
     @Test
